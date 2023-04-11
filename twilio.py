cf53aa064341afb5ed96d496ca98776252ea81b8
Rahul Mohite <41115652+rahul08M@users.noreply.github.com>
2023-04-11 11:32:25 +0530

    Update twilio.py

--- twilio.py
+++ twilio.py
@@ -1,15 +1,15 @@
-from twilio.rest import Client
+from twilio.rest import Client    # Import Client class from twilio module
 
 # Your Account SID from twilio.com/console
-account_sid = "AC99ca7a2839539a376682fb74cbb239e0"
+account_sid = "AC99ca7a2839539a376682fb74cbb239e0"    # account sid
 
 # Your Auth Token from twilio.com/console
-auth_token  = "your_auth_token"
+auth_token  = "your_auth_token"    # auth token
 
-client = Client(account_sid, auth_token)
+client = Client(account_sid, auth_token)    # initialize client object
 
 message = client.messages.create(
-    to="+15558675309",
-    from_="+15017250604",
-    body="Hello from Python!"
+    to="+15558675309",    # receiver mobile number
+    from_="+15017250604",    # sender mobile number
+    body="Hello from Python!"    # message
 )
